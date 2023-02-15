174efdf51d3cc0b908ebfeb9eb34e1c79d6a350c
sanatab <120128180+sanatab@users.noreply.github.com>
2023-02-16 02:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+ function minCost(event){
+  event.preventDefault();
+  var input = document.querySelector("input").value;
+  var arr = input.split(",");
+  arr.sort(function(a,b){return a-b});
+  var cost =0;
+  while(arr.length > 1){
+    var res = Number(arr[0]) + Number(arr[1]);
+    arr.push(res);
+     cost += res;
+     arr.shift();
+     arr.shift();
+     arr.sort(function(a,b){return a-b});
+  }
+  document.getElementById("result").textContent = cost;
+
+}
+
